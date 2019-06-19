ae784adf0d55b611eda3e1f43ef01fc8ab42e498
Youngermaster <jmyoungh@eafit.edu.co>
2019-06-19 10:50:53 -0500

    Stuff

---
+++ Python/Pildoras informaticas/Generators/generators.py
@@ -0,0 +1,24 @@
+# Classic way.
+def classic_even_generator(limit):
+    number = 1
+    evenList = []
+    while number < limit:
+        evenList.append(number * 2)
+        number += 1
+    return evenList
+
+print(classic_even_generator(9))
+
+# Generator way.
+def even_generator(limit):
+    number = 1
+    evenList = []
+    while number < limit:
+        yield number * 2
+        number += 1
+
+getEvens = even_generator(9)
+
+print(next(getEvens))
+print(next(getEvens))
+print(next(getEvens))
