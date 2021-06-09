8762c8b7cb144edcf2050c7b286f66caea9229d5
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-08 21:13:20 -0500

    Undefined behavior examples added

--- C++/undefined_behavior/dangling_reference.cpp
+++ C++/undefined_behavior/dangling_reference.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     cout << "Demo of Undefined Behaviour - dangling reference" << endl;
     cout << "-------------------------------------------------" << endl;
 
@@ -17,13 +16,12 @@ int main(int argc, char const *argv[])
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
-    
-    for (auto &&i : v)
-    {
+
+    for (auto &&i : v) {
         cout << i;
     }
-    
-    int& r1 = v[1];
+
+    int &r1 = v[1];
 
     cout << "\nAddress of V[1] = " << &v[1] << endl;
     cout << "Address of r1 = " << &r1 << endl;
@@ -32,15 +30,13 @@ int main(int argc, char const *argv[])
     v.push_back(4);
     cout << "Push back caused reallocation" << endl;
 
-    for (auto &&i : v)
-    {
+    for (auto &&i : v) {
         cout << i;
     }
 
     cout << "\nAddress of V[1] = " << &v[1] << endl;
     cout << "Address of r1 = " << &r1 << endl;
     cout << "Value of r1 = " << r1 << endl;
-
 
     return 0;
 }
--- C++/undefined_behavior/out_of_bound_index.cpp
+++ C++/undefined_behavior/out_of_bound_index.cpp
@@ -2,24 +2,21 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     cout << "Demo of Undefined Behaviour - out of bounds index" << endl;
     cout << "-------------------------------------------------" << endl;
 
     int array[3]{1, 2, 3};
 
-    for (int i = 0; i <= 3; i++)
-    {
+    for (int i = 0; i <= 3; i++) {
         cout << array[i] << " ";
     }
 
     // ! How to avoid the Undefined Behavior
     cout << endl;
-    for (auto i : array)
-    {
+    for (auto i : array) {
         cout << i << " ";
     }
-    
+    cout << endl;
     return 0;
 }
