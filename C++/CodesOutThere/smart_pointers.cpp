a1cc6b795043e2efc722b171c788d4fa7c5af250
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-10 20:43:53 -0500

    C++ Smart Pointers updated

--- C++/CodesOutThere/smart_pointers.cpp
+++ C++/CodesOutThere/smart_pointers.cpp
@@ -15,10 +15,12 @@ class MyInt {
 
 int main(int argc, char const *argv[]) {
     int *p = new int(10);
-	{
-    MyInt myInt = MyInt(p);
-    cout << *myInt << endl;
-	}
-	cout << *p << endl;
+    {
+        MyInt myInt = MyInt(p);
+        cout << *myInt << endl; // * I should get a 10
+	// ! The pointer value should be deleted after this scope
+    }
+    // ! I won't get a 10, I'll get a default value (0), due to the deletion of the pointer
+    cout << *p << endl; 
     return 0;
 }
