7a2324a8caef14bd8cab4c03b21b8fc61eaea20a
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-25 20:45:46 -0500

    WRY for Rust added

---
+++ Rust/wry/discord_wry/src/main.rs
@@ -0,0 +1,24 @@
+#[allow(non_snake_case)]
+fn main() -> wry::Result<()> {
+    use wry::{
+        application::{
+            event_loop::{ControlFlow, EventLoop},
+            window::WindowBuilder,
+        },
+        webview::WebViewBuilder,
+    };
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Discord Rust")
+        .build(&event_loop)?;
+    let _webview = WebViewBuilder::new(window)?
+        .with_url("https://discord.com/app")?
+        .build()?;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            _ => (),
+        }
+    });
+}
