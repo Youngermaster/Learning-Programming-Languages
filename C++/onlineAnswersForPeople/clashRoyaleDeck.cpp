372f176340bdeb470f86dca736a5690ba980c060
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-26 10:36:25 -0500

    Some sort of scripts added

---
+++ C++/onlineAnswersForPeople/clashRoyaleDeck.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+enum Carta {peeka = "peeka", minero = "minero"};
+
+int main(int argc, char const *argv[]) {
+    string cart;
+    cout << "Cual es tu carta favorita de Clash Royale?: \n";
+    cin >> cart;
+
+    if (cart == "pekka") {
+        cout << "Muy buena carta\n";
+    } else if (cart == "minero") {
+        cout << "Buena carta\n";
+    } else {
+        cout << "No conozco tu carta\n";
+    }
+
+    return 0;
+}
