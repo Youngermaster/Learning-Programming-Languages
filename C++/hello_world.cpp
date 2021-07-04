758591d9254367c6a0ae347c8c95edbbf93a5879
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-03 21:23:50 -0500

    Hello world in different languages

--- C++/hello_world.cpp
+++ C++/hello_world.cpp
@@ -2,5 +2,9 @@
 
 int main() {
 	std::cout << "Hello world 🦀" << std::endl;
+	std::cout << "Olá mundo" << std::endl;
+	std::cout << "Γειά σου Κόσμε" << std::endl;
+	std::cout << "你好，世界" << std::endl;
+	std::cout << "こんにちは世界" << std::endl;
 	std::cout << "Stuff" << std::endl;
 }
