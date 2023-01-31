ccd05b1ca3534456cc0b431677abb6725d951fa6
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-01-31 10:56:33 -0500

    go-react-mongo project finished

---
+++ Go/web-servers/go-react-mongo/client/src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState([])
+
+  async function loadUsers() {
+    const response = await fetch("http://127.0.0.1:3000/users");
+    const data = await response.json();
+    console.log(data)
+    setUsers(data.users)
+  }
+
+  useEffect(() => {
+    loadUsers()
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://127.0.0.1:3000/users", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    loadUsers()
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="name"
+          placeholder="Coloca tu nombre"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <button>Guardar</button>
+      </form>
+
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ Go/web-servers/go-react-mongo/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/youngermaster/go-react-mongo/models"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+	mongodb := os.Getenv("MONGODB_URI")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	if mongodb == "" {
+		mongodb = "mongodb://localhost:27017/gomongodb"
+	}
+
+	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongodb))
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Use(cors.New())
+
+	app.Static("/", "./client/dist")
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user models.User
+
+		c.BodyParser(&user)
+
+		coll := client.Database("gomongodb").Collection("users")
+		result, err := coll.InsertOne(context.TODO(), bson.D{{
+			Key:   "name",
+			Value: user.Name,
+		}})
+
+		if err != nil {
+			panic(err)
+		}
+
+		return c.JSON(&fiber.Map{
+			"data": result,
+		})
+	})
+
+	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+
+		coll := client.Database("gomongodb").Collection("users")
+		results, error := coll.Find(context.TODO(), bson.M{})
+
+		if error != nil {
+			panic(error)
+		}
+
+		for results.Next(context.TODO()) {
+			var user models.User
+			results.Decode(&user)
+			users = append(users, user)
+		}
+
+		return c.JSON(&fiber.Map{
+			"users": users,
+		})
+
+	})
+
+	app.Listen(":" + port)
+	fmt.Println("Server on port 3000")
+}
