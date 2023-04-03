526222147dad0faf5473c743d981356fa5e8345b
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-04-03 14:09:23 -0500

    🐍 FAST API Mongo CRUD finished

--- Python/randomCodes/fast-api-mongo/app/server/app.py
+++ Python/randomCodes/fast-api-mongo/app/server/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+from server.routes.student import router as StudentRouter
+
 app = FastAPI()
+
+app.include_router(StudentRouter, tags=["Student"], prefix="/student")
 
 
 @app.get("/", tags=["Root"])
