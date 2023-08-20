34937f0c0bc413d4f0f485970e8895f2b16585ec
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-08-20 10:07:41 -0500

    🔊 Audio generation with AI added

---
+++ Python/randomCodes/facebook-musicgen-small/music_gen.py
@@ -0,0 +1,28 @@
+import scipy
+from transformers import AutoProcessor, MusicgenForConditionalGeneration
+
+
+processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
+model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
+
+inputs = processor(
+    text=["80s pop track with bassy drums and synth", "90s rock song with loud guitars and heavy drums"],
+    padding=True,
+    return_tensors="pt",
+)
+
+audio_values = model.generate(**inputs, max_new_tokens=256)
+
+sampling_rate = model.config.audio_encoder.sampling_rate
+scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+
+inputs = processor(
+    text=["Drill UK music, agressive beats", "synth drums, reverb"],
+    padding=True,
+    return_tensors="pt",
+)
+
+audio_values = model.generate(**inputs, max_new_tokens=256)
+
+sampling_rate = model.config.audio_encoder.sampling_rate
+scipy.io.wavfile.write("uk_drill_musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
